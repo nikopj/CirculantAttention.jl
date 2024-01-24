dbe77eaf035658cf770ccd4dfd91fd102b0b99ec
Documenter.jl <documenter@juliadocs.github.io>
2024-01-24 21:59:35 +0000

    build based on b086c8f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CirculantAttention","category":"page"},{"location":"#CirculantAttention","page":"Home","title":"CirculantAttention","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CirculantAttention.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CirculantAttention]","category":"page"}]
+}

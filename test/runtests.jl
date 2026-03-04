eed0307f2f00c7aefcf8678a7d69e5423c1c2be7
nikopj <nikola@nyu.edu>
2026-03-04 15:40:27 -0500

    need to mess with CuSparseCSR to get this working

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,13 +16,13 @@ CUDA.allowscalar(false)
 ChainRulesCore.debug_mode() = true
 
 TEST_ELTYPES = (Float32,) #ComplexF32)
-TEST_SPATDIMS = (2,) #2)
+TEST_SPATDIMS = (1,) #2)
 
 include("utils.jl")
 
 # @testset "CirculantAttention.jl" begin
     include("array.jl")
-    include("rrules.jl")
+    # include("rrules.jl")
     # include("attention.jl")
     # include("grad.jl")
 # end

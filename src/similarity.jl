02005a351f9dbf070cb340ef86b8b75af4f1e61a
nikopj <nikola@nyu.edu>
2026-04-22 12:44:55 -0400

    sim kernel without views to fix performance regression

--- src/similarity.jl
+++ src/similarity.jl
@@ -1,3 +1,4 @@
+# NEW SIMILARITY WITH PERFORMANCE REGRESSION 2x OLD Dot SIMILARITY SPEED
 # similarity.jl
 
 abstract type AbstractSimilarity end
@@ -17,11 +18,11 @@ struct RealDotSimilarity <: AbstractSimilarity end
 @inline function simval(::RealDotSimilarity, xv, yv)
     real(xv * conj(yv))
 end
-@inline function simval(::RealDotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, M::Int32) where {Tx, Ty}
+@inline function simval(::RealDotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, Ci, Cj, b, M::Int32) where {Tx, Ty}
     Ts = real(promote_type(Tx, Ty))
     s = zero(Ts)
     @inbounds for m=1i32:M
-        s += real(x[m] * conj(y[m]))
+        s += real(x[Ci, m, b] * conj(y[Cj, m, b]))
     end
     return s
 end
@@ -34,15 +35,15 @@ Distance similarity: ``S_{ij} = -\frac{1}{2}\mathrm{sum}(\mathrm{abs2}, q[i] - k
 struct DistanceSimilarity <: AbstractSimilarity end
 
 @inline function simval(::DistanceSimilarity, xv, yv)
-    - abs2(xv - yv) * 0.5f0
+    - abs2(xv - yv) * typeof(xv)(0.5f0)
 end
-@inline function simval(::DistanceSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, M::Int32) where {Tx, Ty}
+@inline function simval(::DistanceSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, Ci, Cj, b, M::Int32) where {Tx, Ty}
     Ts = real(promote_type(Tx, Ty))
     s = zero(Ts)
     @inbounds for m=1i32:M
-        s -= abs2(x[m] - y[m])
+        s -= abs2(x[Ci, m, b] - y[Cj, m, b])
     end
-    return 0.5f0 * s
+    return Ts(0.5) * s
 end
 
 @doc raw"""
@@ -52,11 +53,11 @@ Phase-invariant dot-product similarity: ``S_{ij} = \lvert q[i]^H k[j] \rvert``.
 """
 struct PIDotSimilarity <: AbstractSimilarity end
 
-@inline function simval(::PIDotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, M::Int32) where {Tx, Ty}
+@inline function simval(::PIDotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, Ci, Cj, b, M::Int32) where {Tx, Ty}
     Ts = promote_type(Tx, Ty)
     s = zero(Ts)
     @inbounds for m=1i32:M
-        s += x[m]*conj(y[m])
+        s += x[Ci, m, b]*conj(y[Cj, m, b])
     end
     return abs(s)
 end
@@ -69,16 +70,16 @@ Phase-invariant distance similarity:
 """
 struct PIDistanceSimilarity <: AbstractSimilarity end
 
-@inline function simval(::PIDistanceSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, M::Int32) where {Tx, Ty}
+@inline function simval(::PIDistanceSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, Ci, Cj, b, M::Int32) where {Tx, Ty}
     Ts = promote_type(Tx, Ty)
     s_xx = zero(real(Ts)); s_xy = zero(Ts); s_yy = zero(real(Ts))
     @inbounds for m=1i32:M
-        xm = x[m]; ym = y[m]
+        xm = x[Ci, m, b]; ym = y[Cj, m, b]
         s_xx += abs2(xm)
         s_xy += xm * conj(ym)
         s_yy += abs2(ym)
     end
-    return -0.5f0 * s_xx + abs(s_xy) - 0.5f0 * s_yy
+    return -real(Ts)(0.5) * s_xx + abs(s_xy) - real(Ts)(0.5) * s_yy
 end
 
 @doc raw"""
@@ -91,11 +92,11 @@ struct DotSimilarity <: AbstractSimilarity end
 @inline function simval(::DotSimilarity, xv, yv)
     xv * conj(yv)
 end
-@inline function simval(sf::DotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, M::Int32) where {Tx, Ty}
+@inline function simval(sf::DotSimilarity, x::AbstractArray{Tx}, y::AbstractArray{Ty}, Ci, Cj, b, M::Int32) where {Tx, Ty}
     Ts = promote_type(Tx, Ty)
     s = zero(Ts)
     @inbounds for m=1i32:M
-        s += x[m]*conj(y[m])
+        s += x[Ci, m, b]*conj(y[Cj, m, b])
     end
     return s
 end
@@ -166,10 +167,7 @@ function circulant_similarity_kernel!(
 
         i, j = cartesian_circulant(n, spatdims, W)
         Ci, Cj = CartInd[i], CartInd[j]
-
-        xj = @view x[Cj, :, b]
-        yi = @view y[Ci, :, b]
-        s = simval(simfun, xj, yi, M)
+        s = simval(simfun, x, y, Ci, Cj, b, M)
 
         S.data.nzVal[n, b] = s
         tid += stride
